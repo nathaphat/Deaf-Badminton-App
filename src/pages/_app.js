58efc89ae823336f8b2ca50bfd9b69334cfea029
nathaphat <kikfriend@gmail.com>
2026-03-27 10:27:34 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,1 +1,15 @@
+import Navbar from '../components/Navbar'; // Import เข้ามา
+import '../styles/globals.css'; // ไฟล์ CSS ของคุณ
 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar /> {/* วางไว้ตรงนี้ เพื่อให้โผล่ทุกหน้า */}
+      <main className="min-h-screen bg-gray-50">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+}
+
+export default MyApp;
