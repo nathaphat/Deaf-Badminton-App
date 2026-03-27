8c77561f90c9f51d15904199f9b5faf1af9b7534
nathaphat <kikfriend@gmail.com>
2026-03-27 16:00:38 +0700

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { signIn, signOut, useSession } from "next-auth/react"
+import { useSession } from "next-auth/react"
 const ProfileLevel = () => {
+  const { data: session } = useSession()
   const [selectedLevel, setSelectedLevel] = useState('หน้าบ้าน');
 
   const levels = [
