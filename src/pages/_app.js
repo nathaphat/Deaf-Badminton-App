56543b476f02c75793e003b6c8fa359dd656d34d
nathaphat <kikfriend@gmail.com>
2026-03-27 11:07:53 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,17 @@ import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-column">
       <Navbar />
-      <main className="max-w-4xl mx-auto p-4">
+      
+      <main className="max-w-4xl mx-auto p-4 flex-grow">
         <Component {...pageProps} />
       </main>
+
+      {/* ย้ายมาไว้ข้างล่างสุด จะดูเป็นระเบียบกว่าครับ */}
+      <footer className="text-center p-4 text-gray-500 text-sm">
+        My app by Kik
+      </footer>
     </div>
   );
 }
