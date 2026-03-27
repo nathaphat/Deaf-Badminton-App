ebcf7100d6fcc5cc4c6e3d5ca94de94bca831fb5
nathaphat <kikfriend@gmail.com>
2026-03-27 10:55:23 +0700

    Update finance.js

--- src/pages/finance.js
+++ src/pages/finance.js
@@ -1,1 +1,10 @@
+const FinancePage = () => {
+  return (
+    <div>
+      <h1>ระบบการเงิน</h1>
+      <p>กำลังพัฒนา...</p>
+    </div>
+  );
+};
 
+export default FinancePage; // ต้องมีบรรทัดนี้เสมอ!
