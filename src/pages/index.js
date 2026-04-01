1396592a9ec80615d6680cd32751d3b659f207fe
nathaphat <kikfriend@gmail.com>
2026-04-01 16:29:52 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -101,6 +101,12 @@ return (
 
     {/* ปุ่มทดสอบ (เอาออกได้เมื่อสวยแล้ว) */}
     <button className="hidden bg-red-500 text-white p-4 rounded-xl mt-4">TEST TAILWIND</button>
+    <button 
+      style={{ border: '10px solid blue' }} 
+      className="bg-red-500 text-white p-10 text-5xl"
+    >
+    TEST TAILWIND
+    </button>
   </div>
 )
 }
