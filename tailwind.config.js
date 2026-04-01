6ba488760b06a2bd743ed1e937332386ac1c00f4
nathaphat <kikfriend@gmail.com>
2026-04-01 16:20:37 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./styles/**/*.{js,ts,jsx,tsx,css}",
+    "./index.js",
+    "./_app.js"
   ],
   theme: {
     extend: {},
