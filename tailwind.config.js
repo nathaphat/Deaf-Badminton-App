538ee42099268f2426efb4df82a87ef3c23c8b62
nathaphat <kikfriend@gmail.com>
2026-04-01 14:08:10 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",      // 1. ชี้ไปที่โฟลเดอร์ pages นอก src
-    "./components/**/*.{js,ts,jsx,tsx,mdx}", // 2. ชี้ไปที่โฟลเดอร์ components นอก src
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",        // 3. กวาดใน src ไว้ด้วยเผื่อมีไฟล์อื่น
-    "./styles/**/*.css",                     // 4. กวาดไฟล์ css
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
