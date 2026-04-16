e67b7b9e551bfbac123b48a53ba2fb6add85a4ec
nathaphat <kikfriend@gmail.com>
2026-04-16 10:32:53 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/logic/**/*.{js,ts,jsx,tsx,mdx}", // เพิ่มบรรทัดนี้ถ้ามีการใช้ Tailwind ใน logic
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",      // หรือใช้แบบนี้เพื่อครอบคลุมทุกอย่างใน src
+    "./src/**/*.{js,ts,jsx,tsx,mdx}", // บรรทัดนี้สำคัญที่สุด: มันสั่งให้ Tailwind ไปหา class ในทุกไฟล์ที่อยู่ในโฟลเดอร์ src
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 ​
