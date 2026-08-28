64253ca816279de0b3dc62b92653676850b57da7
nathaphat <kikfriend@gmail.com>
2026-08-28 16:28:17 +0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
 import Navbar from '../components/Navbar';
-import Navbar from '../components/Footer';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
